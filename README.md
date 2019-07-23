ce2758d5c5528227d4dd983dfdb906e1dd95b637
Viktors Telle <viktors.telle@if.lv>
2019-07-23 13:12:22 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Prototype of API using GRPC (https://docs.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-3.0&tabs=visual-studio-code).
+
+Back-end is built using .NET Core 3.0.
---
+++ backend/GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcTestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+                true);
+            var httpClient = new HttpClient {BaseAddress = new Uri("http://localhost:50051")};
+            // The port number(50051) must match the port of the gRPC server.
+            var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
